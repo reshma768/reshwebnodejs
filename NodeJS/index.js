0147ed5667be88b8845b01665965ce43fd08ae85
reshma768 <144494726+reshma768@users.noreply.github.com>
2023-11-22 14:29:23 -0500

    Add files via upload

---
+++ NodeJS/index.js
@@ -0,0 +1,45 @@
+const http = require('http') ; 
+const fs = require('fs');
+const path = require('path');
+http.createServer((req, res) =>{
+    console.log(req.url);
+    if(req.url ==='/'){ // home page
+
+       fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+
+        if (err) throw err ;
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(content)
+
+       })
+
+    }
+    else if (req.url === '/about'){
+        fs.readFile(path.join(__dirname,'public','about.html'),(err,content)=>{
+
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(content)
+    
+           })
+    
+    }
+    else if (req.url ==='/api'){
+        fs.readFile(path.join(__dirname,'public','db.json'),(err,content)=>{
+
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(content)
+    
+           })
+    
+    }
+    else{
+        res.end("<h1> 404 Nothing is here </h1>")
+    }
+
+    
+
+}).listen(5959,()=>console.log("Server is running"));
+
+ 
